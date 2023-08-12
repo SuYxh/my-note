6df571496783fd7d6c81f22182f003b915484314
yangxinhao <yangxinhao@zhuanzhuan.com>
2023-08-13 00:20:53 +0800

    feat: init

---
+++ docs/nav/data.ts
@@ -0,0 +1,64 @@
+import type { NavData } from "../.vitepress/theme/types";
+
+export const NAV_DATA: NavData[] = [
+  {
+    title: "常用工具",
+    items: [
+      {
+        icon: "https://caniuse.com/img/favicon-128.png",
+        title: "Can I use",
+        desc: "前端 API 兼容性查询",
+        link: "https://caniuse.com",
+      },
+      {
+        icon: "https://tinypng.com/images/apple-touch-icon.png",
+        title: "TinyPNG",
+        desc: "在线图片压缩工具",
+        link: "https://tinypng.com",
+      },
+      {
+        icon: "https://devtool.tech/logo.svg",
+        title: "开发者武器库",
+        desc: "开发者武器库，做开发者最专业最好用的专业工具箱",
+        link: "https://devtool.tech",
+      },
+      {
+        icon: "https://tool.lu/favicon.ico",
+        title: "在线工具",
+        desc: "开发人员的工具箱",
+        link: "https://tool.lu",
+      },
+      {
+        icon: "/icons/json-cn.ico",
+        title: "Json 中文网",
+        desc: "JSON 在线解析及格式化验证",
+        link: "https://www.json.cn",
+      },
+    ],
+  },
+  {
+    title: "AI 导航",
+    items: [
+      {
+        icon: "/icons/chatgpt.png",
+        title: "ChatGPT（最强）",
+        link: "https://chat.openai.com/chat",
+      },
+      {
+        icon: "https://www.notion.so/images/logo-ios.png",
+        title: "Notion AI（笔记）",
+        link: "https://www.notion.so",
+      },
+      {
+        icon: "https://www.midjourney.com/apple-touch-icon.png",
+        title: "Midjourney（绘画）",
+        link: "https://www.midjourney.com",
+      },
+      {
+        icon: "https://global-uploads.webflow.com/59deb588800ae30001ec19c9/5d4891e0e260e3c1bc37b100_beautiful%20ai%20favicon%20%20blue%20square.png",
+        title: "Beautiful.ai（PPT）",
+        link: "https://www.beautiful.ai",
+      },
+    ],
+  },
+];
