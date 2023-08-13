508ffeb86787e5be2afa8cd5d31af02a61c9e117
yangxinhao <yangxinhao@zhuanzhuan.com>
2023-08-13 19:34:10 +0800

    feat: 项目整理

--- docs/nav/data.ts
+++ docs/nav/data.ts
@@ -2,6 +2,29 @@ import type { NavData } from "../.vitepress/theme/types";
 
 export const NAV_DATA: NavData[] = [
   {
+    title: "GPT 项目",
+    items: [
+      {
+        icon: "https://qn.huat.xyz/mac/202308131235206.png",
+        title: "ChatGPT Web",
+        desc: "支持 GPT4",
+        link: "https://gpt-plus.huat.xyz/",
+      },
+      {
+        icon: "https://qn.huat.xyz/mac/202308131235206.png",
+        title: "ChatGPT Web Admin",
+        desc: "ChatGPT Web的后台管理系统，支持 RBAC",
+        link: "https://gpt-admin.huat.xyz/",
+      },
+      {
+        icon: "https://qn.huat.xyz/mac/202308131235206.png",
+        title: "ChatGPT Web API",
+        desc: "基于 Express 、 MySQL",
+        link: "https://openai.huat.xyz/",
+      },
+    ],
+  },
+  {
     title: "文档相关",
     items: [
       {
@@ -25,27 +48,83 @@ export const NAV_DATA: NavData[] = [
     ],
   },
   {
+    title: "基础建设",
+    items: [
+      {
+        icon: "https://qn.huat.xyz/mac/202308131235206.png",
+        title: "脚手架",
+        desc: "正在开发中",
+        link: "https://fe-nav-five.vercel.app/",
+      },
+      {
+        icon: "https://qn.huat.xyz/mac/202308131235206.png",
+        title: "工具库",
+        desc: "开发中常用的工具函数",
+        link: "https://super-zuccutto-ae4aaf.netlify.app/",
+      },
+      {
+        icon: "https://qn.huat.xyz/mac/202308131235206.png",
+        title: "UI 库",
+        desc: "一个破烂的 UI",
+        link: "https://www.npmjs.com/package/iron-ui",
+      },
+    ],
+  },
+  {
+    title: "项目模板",
+    items: [
+      {
+        icon: "https://qn.huat.xyz/mac/202308131235206.png",
+        title: "vitepress 模板",
+        desc: "开箱即用的文档网站",
+        link: "https://fe-nav-five.vercel.app/",
+      },
+      {
+        icon: "https://qn.huat.xyz/mac/202308131235206.png",
+        title: "Vue Admin",
+        desc: "vue 版本的后台管理项目",
+        link: "https://fe-interview-sigma.vercel.app/",
+      },
+      {
+        icon: "https://qn.huat.xyz/mac/202308131235206.png",
+        title: "移动端项目",
+        desc: "我的笔记",
+        link: "https://my-note-nu.vercel.app/",
+      },
+      {
+        icon: "https://qn.huat.xyz/mac/202308131235206.png",
+        title: "Eggjs 模板",
+        desc: "Eggjs 模板",
+        link: "https://my-note-nu.vercel.app/",
+      },
+      {
+        icon: "https://qn.huat.xyz/mac/202308131235206.png",
+        title: "Express 模板",
+        desc: "Express 模板",
+        link: "https://my-note-nu.vercel.app/",
+      },
+    ],
+  },
+  {
     title: "微信相关",
     items: [
       {
-        icon: "/icons/chatgpt.png",
-        title: "ChatGPT（最强）",
+        icon: "https://qn.huat.xyz/mac/202308122340353.jpg",
+        title: "题库小程序",
+        desc: "有一些题目，待完善",
         link: "https://chat.openai.com/chat",
       },
       {
-        icon: "https://www.notion.so/images/logo-ios.png",
-        title: "Notion AI（笔记）",
-        link: "https://www.notion.so",
+        icon: "https://qn.huat.xyz/mac/202308131235206.png",
+        title: "题库-管理后台",
+        desc: "正在整理中",
+        link: "--",
       },
       {
-        icon: "https://www.midjourney.com/apple-touch-icon.png",
-        title: "Midjourney（绘画）",
-        link: "https://www.midjourney.com",
-      },
-      {
-        icon: "https://global-uploads.webflow.com/59deb588800ae30001ec19c9/5d4891e0e260e3c1bc37b100_beautiful%20ai%20favicon%20%20blue%20square.png",
-        title: "Beautiful.ai（PPT）",
-        link: "https://www.beautiful.ai",
+        icon: "https://qn.huat.xyz/mac/202308131235206.png",
+        title: "题库-API",
+        desc: "正在整理中",
+        link: "--",
       },
     ],
   },
