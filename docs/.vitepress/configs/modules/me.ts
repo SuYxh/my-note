4ce7414aabc345d43c219bb9d539a2d616437dca
yangxinhao <yangxinhao@zhuanzhuan.com>
2023-11-05 22:38:01 +0800

    feat: update

--- docs/.vitepress/configs/modules/me.ts
+++ docs/.vitepress/configs/modules/me.ts
@@ -1,25 +1,30 @@
+// const data = {
+//   text: "Me",
+//   items: [
+//     {
+//       text: "面试",
+//       link: "/interview/index",
+//       activeMatch: "^/interview",
+//     },
+//     {
+//       text: "interview",
+//       link: "https://fe-interview-sigma.vercel.app/",
+//     },
+//     {
+//       text: "语雀",
+//       link: "https://www.yuque.com/jarvis-zzzhw/frontend?# 《Front-End》",
+//     },
+//     {
+//       text: "日常笔记",
+//       link: "https://github.com/SuYxh/my-note",
+//     },
+//     { text: "掘金", link: "https://juejin.cn/user/2084329779636094/posts" },
+//   ],
+// };
+
 const data = {
-  text: "Me",
-  items: [
-    {
-      text: "面试",
-      link: "/interview/index",
-      activeMatch: "^/interview",
-    },
-    // {
-    //   text: "interview",
-    //   link: "https://fe-interview-sigma.vercel.app/",
-    // },
-    {
-      text: "个人主页",
-      link: "https://www.yuque.com/jarvis-zzzhw/frontend?# 《Front-End》",
-    },
-    {
-      text: "日常笔记",
-      link: "https://github.com/SuYxh/my-note",
-    },
-    { text: "掘金", link: "https://juejin.cn/user/2084329779636094/posts" },
-  ],
+  text: "我的语雀",
+  link: "https://www.yuque.com/jarvis-zzzhw/frontend?# 《Front-End》",
 };
 
 export default data;
